8f1116bf547ec1855476f3e78a42ee052344e684
Chandani Kumari <chandaanikumari92@gmail.com>
2023-07-29 13:06:46 -0700

    Co-authored-by: dhandrohit@hotmail.com <dhandrohit@hotmail.com>

---
+++ head.js
@@ -0,0 +1,20 @@
+// FUNCTION IMPLEMENTATION
+const _ = require('lodash');
+// import * as _ from 'lodash';
+// module.exports = {'lodash'};
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed:", {actual}, "===", {expected});
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed:", {actual}, "!==", {expected});
+  }
+};
+const head = function(arr) {
+  let firstElement = _.head(arr);
+  return firstElement;
+};
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]),);
