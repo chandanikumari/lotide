27417cc3eeebfdca388b149823e16c617a4bbcf9
Chandani Kumari <chandaanikumari92@gmail.com>
2023-07-30 16:45:34 -0700

    Added tail.js

---
+++ tail.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed:", {actual}, "===", {expected});
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed:", {actual}, "!==", {expected});
+  }
+};
+  
+const tail = function(arr) {
+  return arr.slice(1);
+};
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+const word1 = [1,2,3,4,5,6,7];
+
+console.log(tail(words)); // no need to capture the return value since we are not checking it
+console.log(tail(word1));
+assertEqual(words.length, 3);
+assertEqual(word1.length, 7);
