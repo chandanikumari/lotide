518dd01026170c5831f9705d257034748039a017
Chandani Kumari <chandaanikumari92@gmail.com>
2023-07-30 22:20:30 -0700

    updated the test cases

--- countOnly.js
+++ countOnly.js
@@ -36,9 +36,10 @@ const firstNames = [
   "Joe"
 ];
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false , "Salima" : true});
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
+assertEqual(result1["Salima"], 2);
