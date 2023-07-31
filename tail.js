ef35052f1157fe8068973926d720a2bef02b055e
Chandani Kumari <chandaanikumari92@gmail.com>
2023-07-30 18:36:06 -0700

    updated tail.js

--- tail.js
+++ tail.js
@@ -12,7 +12,7 @@ const tail = function(arr) {
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 const word1 = [1,2,3,4,5,6,7];
 
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-console.log(tail(word1));
+tail(words); // no need to capture the return value since we are not checking it
+tail(word1);
 assertEqual(words.length, 3);
 assertEqual(word1.length, 7);
