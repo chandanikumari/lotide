edc63f25a44a1e4f2bc95f88d2ef5ce3211f9ec1
Chandani Kumari <chandaanikumari92@gmail.com>
2023-07-31 19:55:17 -0700

    Refactored eqArrays and eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,23 +1,11 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed:", {actual}, "===", {expected});
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed:", {actual}, "!==", {expected});
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(actual, expected) {
   if (actual.toString() === expected.toString()) {
-    console.log("True it is!");
     return true;
   }
-  console.log("False it is!");
   return false;
 };
 
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true;
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
