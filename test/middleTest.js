c436187ed9af6c4321a525fddc2f9138f6ec5ff7
Chandani Kumari <chandaanikumari92@gmail.com>
2023-08-03 01:10:43 -0700

    updated middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -13,7 +13,7 @@ describe("#middle", () => {
 
   it("returns [3] for [1, 2, 3, 4, 5]", () => {
     console.log(middle([1, 2, 3, 4, 5]));
-    assert.strictEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
+    assert.deepEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
   });
 });
 
