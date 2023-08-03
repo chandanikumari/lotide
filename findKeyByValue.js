e468a874a945a42917d0b72705c29b3fd11cf7c1
Chandani Kumari <chandaanikumari92@gmail.com>
2023-08-02 22:51:56 -0700

    implemented findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,22 @@
+const { values } = require("lodash");
+const assertEqual = require("./assertEqual");
+
+
+const findKeyByValue = function(obj, show) {
+  for (const ob in obj) {
+    if (obj[ob] === show) {
+      return ob;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  "sci_fi": "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+const keys = Object.keys(bestTVShowsByGenre);
+console.log(keys);
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
