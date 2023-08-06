ed0462a6a2df80f77745d1bb7db54d0aaeff45d7
Chandani Kumari <chandaanikumari92@gmail.com>
2023-08-06 15:34:13 -0700

    Implemented eqObject

---
+++ eqObjects.js
@@ -0,0 +1,23 @@
+const assertEqual = require("./assertEqual");
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let key of Object.keys(object1)) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true); // => true
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false); // => false
